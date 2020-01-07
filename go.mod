6da22102b86e0b9bad8a97dbe6c1ee8443be1337
Elton Minetto <eminetto@gmail.com>
2020-01-07 08:28:10 -0300

    wip

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/eminetto/pos-web-go
 
 go 1.13
 
-require github.com/mattn/go-sqlite3 v2.0.2+incompatible // indirect
+require (
+	github.com/codegangsta/negroni v1.0.0 // indirect
+	github.com/gorilla/mux v1.7.3 // indirect
+	github.com/mattn/go-sqlite3 v2.0.2+incompatible // indirect
+)
--- web/main.go
+++ web/main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	"github.com/codegangsta/negroni"
+	"github.com/eminetto/pos-web-go/core/beer"
+	"log"
+	_ "github.com/mattn/go-sqlite3"
+	"net/http"
+	"os"
+	"time"
+	"github.com/gorilla/mux"
+)
 
+func main() {
+	db, err := sql.Open("sqlite3", "data/beer.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	service := beer.NewService(db)
+	/*mux := http.NewServeMux()
+	mux.Handle("/", hello(service))*/
+	r := mux.NewRouter()
+	//handlers
+	n := negroni.New(
+		negroni.NewLogger(),
+	)
+	r.Handle("/v1/beer", n.With(
+		negroni.Wrap(hello(service)),
+	)).Methods("GET", "OPTIONS")
+	http.Handle("/", r)
+
+	logger := log.New(os.Stderr, "logger: ", log.Lshortfile)
+	srv := &http.Server{
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		Addr:         ":4000",
+		//Handler:      mux,
+		Handler:      http.DefaultServeMux,
+		ErrorLog:     logger,
+	}
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func hello(service *beer.Service) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		all, _ := service.GetAll()
+		fmt.Println(all)
+	})
 }
