5f51f41202fc3416f2340f780cda425e3afb0fce
Elton Minetto <elton.minetto@betrybe.com>
2021-03-22 22:04:25 -0300

    fix: tests

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/codegangsta/negroni v1.0.0
 	github.com/gorilla/mux v1.8.0
 	github.com/mattn/go-sqlite3 v1.14.6
+	github.com/stretchr/testify v1.7.0
 )
--- web/handlers/beer_test.go
+++ web/handlers/beer_test.go
@@ -3,13 +3,14 @@ package handlers
 import (
 	"database/sql"
 	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
 	"github.com/eminetto/pos-web-go/core/beer"
 	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/assert"
-	"net/http"
-	"net/http/httptest"
-	"testing"
 )
 
 func Test_getAllBeer(t *testing.T) {
@@ -27,20 +28,23 @@ func Test_getAllBeer(t *testing.T) {
 	}
 	db, err := sql.Open("sqlite3", "../../data/beer_test.db")
 	assert.Nil(t, err)
+	assert.Nil(t, clearDB(db))
 	service := beer.NewService(db)
 	assert.Nil(t, service.Store(b1))
 	_ = service.Store(b2)
 	handler := getAllBeer(service)
 	r := mux.NewRouter()
 	r.Handle("/v1/beer", handler)
-	ts := httptest.NewServer(r)
-	defer ts.Close()
-	resp, err := http.Get(ts.URL + "/v1/beer")
+	req, err := http.NewRequest("GET", "/v1/beer", nil)
 	assert.Nil(t, err)
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	req.Header.Set("Accept", "application/json")
+
+	rr := httptest.NewRecorder()
+	r.ServeHTTP(rr, req)
+	assert.Equal(t, http.StatusOK, rr.Code)
 
 	var result []*beer.Beer
-	err = json.NewDecoder(resp.Body).Decode(&result)
+	err = json.NewDecoder(rr.Body).Decode(&result)
 	assert.Nil(t, err)
 	assert.Equal(t, 2, len(result))
 	assert.Equal(t, b1.ID, result[0].ID)
@@ -91,14 +95,18 @@ func Test_getAllBeerWithMock(t *testing.T) {
 	handler := getAllBeer(service)
 	r := mux.NewRouter()
 	r.Handle("/v1/beer", handler)
-	ts := httptest.NewServer(r)
-	defer ts.Close()
-	resp, err := http.Get(ts.URL + "/v1/beer")
+
+	req, err := http.NewRequest("GET", "/v1/beer", nil)
 	assert.Nil(t, err)
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	req.Header.Set("Accept", "application/json")
+
+	rr := httptest.NewRecorder()
+	r.ServeHTTP(rr, req)
+	assert.Equal(t, http.StatusOK, rr.Code)
+
 
 	var result []*beer.Beer
-	err = json.NewDecoder(resp.Body).Decode(&result)
+	err = json.NewDecoder(rr.Body).Decode(&result)
 	assert.Nil(t, err)
 	assert.Equal(t, 2, len(result))
 	assert.Equal(t, int64(10), result[0].ID)
@@ -110,14 +118,27 @@ func Test_getBeerWithMock(t *testing.T) {
 	handler := getBeer(service)
 	r := mux.NewRouter()
 	r.Handle("/v1/beer/{id}", handler)
-	ts := httptest.NewServer(r)
-	defer ts.Close()
-	resp, err := http.Get(ts.URL + "/v1/beer/10")
-	assert.Nil(t, err)
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
 
+	req, err := http.NewRequest("GET", "/v1/beer/10", nil)
+	assert.Nil(t, err)
+	req.Header.Set("Accept", "application/json")
+
+	rr := httptest.NewRecorder()
+	r.ServeHTTP(rr, req)
+	assert.Equal(t, http.StatusOK, rr.Code)
+	
 	var result *beer.Beer
-	err = json.NewDecoder(resp.Body).Decode(&result)
+	err = json.NewDecoder(rr.Body).Decode(&result)
 	assert.Nil(t, err)
 	assert.Equal(t, int64(10), result.ID)
+}
+
+func clearDB(db *sql.DB) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	_, err = tx.Exec("delete from beer")
+	tx.Commit()
+	return err
 }
